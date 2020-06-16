fc00865e2af55b4480eee8ae17fa41a8ada926cb
Jimmy Wang <jimmy.wang123@outlook.sg>
2020-06-16 10:03:19 +0800

    get user input on water consume actions

---
+++ constants.py
@@ -0,0 +1,10 @@
+# Constants to avoid errors from mistyping strings
+
+# Things that consume water
+FLUSH = 'flush'
+SHOWER = 'shower'
+BATH = 'bath'
+DRINK = 'drink'
+LAUNDRY = 'laundry'
+
+actions = FLUSH, SHOWER, BATH, DRINK, LAUNDRY
---
+++ entrypoint.py
@@ -0,0 +1,8 @@
+from constants import *
+
+def main():
+    for action in actions:
+        input("%s? " % action)
+
+if __name__ == '__main__':
+    main()
