c9406ff3fefb79d3dec75fc8cc5f5d17e0238231
Jimmy Wang <jimmy.wang123@outlook.sg>
2020-06-18 12:13:27 +0800

    remove optimal dict

--- backend/data.py
+++ backend/data.py
@@ -9,25 +9,13 @@ TIMES = 'times'
 MINUTES = 'minutes'
 LITRES = 'litres'
 
-# The optimal amounts
-
-# https://www.webmd.com/urinary-incontinence-oab/rm-quiz-urine-normal
-# https://www.pub.gov.sg/Documents/PUB_7%20Good%20water%20saving%20habits_FA_LORES.pdf
-# https://www.thefinder.com.sg/healthy-living/fitness/how-much-water-must-you-actually-drink-singapores-insane-hot/
-
-optimal = {
-    FLUSH: 8,
-    SHOWER: 5,
-    DRINK: 2.25,
-}
-
 
 class UserAction:
-    def __init__(self, action, query, advice_format, unit):
-        self.action = action
+    def __init__(self, query: str, advice_format: str, optimal, unit):
         self.user_val = None
         self.query = query
         self.advice_format = advice_format
+        self.optimal = optimal
         self.unit = unit
 
     def set_user_val_on_cmdline(self):
@@ -37,25 +25,24 @@ class UserAction:
         self.user_val = float(user_val)
 
     @property
-    def optimal(self):
-        return optimal[self.action]
-
-    @property
     def advice(self):
         return self.advice_format.format(self.optimal)
 
 
+# The optimal amounts
+
+# https://www.webmd.com/urinary-incontinence-oab/rm-quiz-urine-normal
+# https://www.pub.gov.sg/Documents/PUB_7%20Good%20water%20saving%20habits_FA_LORES.pdf
+# https://www.thefinder.com.sg/healthy-living/fitness/how-much-water-must-you-actually-drink-singapores-insane-hot/
+
 actions = (
-    UserAction(FLUSH,
-               'How many times do you flush your toilet per day?',
+    UserAction('How many times do you flush your toilet per day?',
                'Try flushing the toilet {} times tomorrow.',
-               TIMES),
-    UserAction(SHOWER,
-               'How much time do you usually spend in the shower?',
+               8, TIMES),
+    UserAction('How much time do you usually spend in the shower?',
                'Shorten your shower time to {} minutes in the future.',
-               MINUTES),
-    UserAction(DRINK,
-               'How many litres of water do you drink per day?',
+               5, MINUTES),
+    UserAction('How many litres of water do you drink per day?',
                'Unless you perspire a lot, drink {} litres of water tomorrow.',
-               LITRES),
+               2.25, LITRES),
 )
