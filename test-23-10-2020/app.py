ed17d8f4a4c91f504092eb517c30886aa13ec26b
jimmy-print <jimmy.wang123@outlook.sg>
2020-11-02 11:32:59 +0800

    fix regular get request

--- test-23-10-2020/app.py
+++ test-23-10-2020/app.py
@@ -6,7 +6,10 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     gen = flask.request.form.values()
-    anything = tuple(gen)[0]
+    try:
+        anything = tuple(gen)[0]
+    except:
+        return flask.render_template('index.html')
     print(anything, nlp.return_action_type(anything))
     return flask.render_template('index.html')
 
